1970e69cea195f5755d10a54d3b23d882447d667
KoleSchramm <kole.schramm@csuglobal.edu>
2026-04-04 23:35:42 -0500

    Second Commit

--- app/src/main/java/com/example/unitconverter4/ConversionType.kt
+++ app/src/main/java/com/example/unitconverter4/ConversionType.kt
@@ -1,7 +1,7 @@
 package com.example.unitconverter4
 
 enum class ConversionType(val label: String) {
-    INCHES_TO_CM("Inches to Centimeters"),
+    INCHES_TO_CM("Inches to Centimeters this is a change"),
     CM_TO_INCHES("Centimeters to Inches"),
     FAHRENHEIT_TO_CELSIUS("Fahrenheit to Celsius"),
     CELSIUS_TO_FAHRENHEIT("Celsius to Fahrenheit")
